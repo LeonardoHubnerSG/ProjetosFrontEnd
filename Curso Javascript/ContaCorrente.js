22326a600858af5a572eb70aa02409aee7486a99
Leonardo Hubner <leonardo.hubner@sgsistemas.com.br>
2022-10-18 15:15:46 -0300

    Curso Javascript inicio projeto Contas (Corrente,Poupança)

---
+++ Curso Javascript/ContaCorrente.js
@@ -0,0 +1,20 @@
+import {Cliente} from "./Cliente.js";
+import {Conta} from "./Conta.js";
+
+export class ContaCorrente extends Conta{
+    static numeroContas = 0;
+    // agencia;
+    // _saldo = 0;
+    // _cliente;
+
+    constructor (agencia, cliente){
+        super(100, agencia, cliente)
+        ContaCorrente.numeroContas ++;
+    }
+
+    sacar(valor){
+        let taxa = valor * 0.1;
+        valor += taxa;
+        return super.sacar(valor);
+    }
+}
---
+++ Curso Javascript/ContaPoupanca.js
@@ -0,0 +1,20 @@
+import {Cliente} from "./Cliente.js";
+import {Conta} from "./Conta.js";
+
+export class ContaPoupanca extends Conta{
+    static numeroContas = 0;
+    // agencia;
+    // _saldo = 0;
+    // _cliente;
+
+    constructor (agencia, cliente){
+        super(100, agencia, cliente)
+        ContaPoupanca.numeroContas ++;
+    }
+
+    sacar(valor){
+        let taxa = valor * 0.05;
+        valor += taxa;
+        return super.sacar(valor);
+    }
+}
